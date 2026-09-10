d1463646f345140a240e97911996e747ec57d5af
Bastien Lepesant <bastien@lepesant.me>
2026-09-10 16:57:22 +0200

    Fix build with androidx core version

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core:1.19.0")
+    implementation("androidx.core:core:1.15.0")
     implementation("org.godotengine:godot:4.5.1.stable")
     // TODO: Additional dependencies should be added to export_plugin.gd as well.
 }
